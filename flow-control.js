5ac0e13d73aeb887cf25b21fcbd2e10a9bc57428
Elias Felvinczi <eliasfelvinczi@gmail.com>
2020-05-07 11:55:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,19 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age <= 19 ) {
+    return 'You are a teenager!';
+  }
 }
 
 function teenager(age) {
-
+  if (age < 13 || age > 19 ) {
+    return 'You are not a teenager';
+  }
 }
 
 function ageChecker(age) {
-
+  if (age <= 12) {
+    return 'You are a kid';
+  }
 }
 
 function ternaryTeenager(age) {
